fdaca027a427d95bd29c2d252621bf6ab7a533ef
GaaraSan <prosto.artem158@gmail.com>
2023-11-18 14:19:56 +0200

    short-info jQuery

--- pr7/src/js/short-info.js
+++ pr7/src/js/short-info.js
@@ -1,6 +1,6 @@
-const shorts_button = document.querySelector(".shorts");
-const shorts_info = document.querySelector("aside");
+const shorts_button = $('.shorts')
+const shorts_info = $('aside')[0]
 
-shorts_button.addEventListener("click", () => {
-  shorts_info.classList.toggle("shorts-hiden");
-});
+shorts_button.on('click', () => {
+	shorts_info.classList.toggle('shorts-hiden')
+})
