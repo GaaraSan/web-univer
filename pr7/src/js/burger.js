68230b5ff81dab6183e981f5f5c7c17f832c5aaa
GaaraSan <prosto.artem158@gmail.com>
2023-11-18 13:37:03 +0200

    burger jQuery

--- pr7/src/js/burger.js
+++ pr7/src/js/burger.js
@@ -1,10 +1,5 @@
-const burger_button = document.querySelector(".burger");
-const burger_main = document.querySelector(".main-page");
-const burger_bio = document.querySelector(".biography-page");
-const burger_achiev = document.querySelector(".achievements-page");
-const burger_books = document.querySelector(".books-page");
-const nav_hiden = document.querySelector(".nav-hiden");
+const nav_hiden = $('.nav-hiden')[0]
 
-burger_button.addEventListener("click", () => {
-  nav_hiden.classList.toggle("nav-hiden");
-});
+$('.burger').on('click', () => {
+	nav_hiden.classList.toggle('nav-hiden')
+})
