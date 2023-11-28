e24162c293923924ed4c0c3fc6fed5354091e0f4
GaaraSan <prosto.artem158@gmail.com>
2023-11-28 20:39:15 +0200

    deleted all files and created lab8.docx

--- pr8/src/js/burger.js
+++
@@ -1,5 +0,0 @@
-const nav_hiden = $('.nav-hiden')[0]
-
-$('.burger').on('click', () => {
-	nav_hiden.classList.toggle('nav-hiden')
-})
--- pr8/src/js/short-info.js
+++
@@ -1,6 +0,0 @@
-const shorts_button = $('.shorts')
-const shorts_info = $('aside')[0]
-
-shorts_button.on('click', () => {
-	shorts_info.classList.toggle('shorts-hiden')
-})
